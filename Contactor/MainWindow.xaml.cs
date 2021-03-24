c1dfc65f77bf05ccc805a1d2393116a19f268c5c
Kiwi <hallerlucas@outlook.com>
2021-03-24 16:29:38 +0100

    Creating data table

--- Contactor/MainWindow.xaml.cs
+++ Contactor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,22 @@ namespace Contactor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class DataObject
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string Mail { get; set; }
+            public string Phone { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            var list = new ObservableCollection<DataObject>();
+            list.Add(new DataObject() { FirstName = "John", LastName = "Doe", Age = 35, Mail = "john.doe@example.com", Phone = "0101 010 101" });
+            this.ContactList.ItemsSource = list;
         }
+        
     }
 }
