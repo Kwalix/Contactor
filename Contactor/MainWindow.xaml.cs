d6d89edc18548fecb44ee3cc10f62f9dceea49cd
Kiwi <hallerlucas@outlook.com>
2021-03-25 16:33:35 +0100

    Added image file, and creating data base creation methods

--- Contactor/MainWindow.xaml.cs
+++ Contactor/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiteDB;
+using Microsoft.Win32;
 
 namespace Contactor
 {
@@ -21,6 +24,7 @@ namespace Contactor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string DBPath = String.Empty;
         public class DataObject
         {
             public string FirstName { get; set; }
@@ -33,10 +37,43 @@ namespace Contactor
         public MainWindow()
         {
             InitializeComponent();
-            var list = new ObservableCollection<DataObject>();
-            list.Add(new DataObject() { FirstName = "John", LastName = "Doe", Age = 35, Mail = "john.doe@example.com", Phone = "0101 010 101" });
-            this.ContactList.ItemsSource = list;
-        }
+            if (UserPreferences.Default.LastDBPath != "")
+            {
+                this.Title = UserPreferences.Default.LastDBPath;
+            }
+        /// var list = new ObservableCollection<DataObject>();
+        /// list.Add(new DataObject() { FirstName = "John", LastName = "Doe", Age = 35, Mail = "john.doe@example.com", Phone = "0101 010 101" });
+        /// this.ContactList.ItemsSource = list;
         
+    }
+
+        private void AddUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NewDB_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Create New DataBase";
+            saveFileDialog.DefaultExt = ".db";
+            saveFileDialog.Filter = "Data Base File (.db) | *.db";
+
+            if (saveFileDialog.ShowDialog() == true)
+                Console.WriteLine("coucou");
+        }
+
+        private void OpenDB_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Open DataBase";
+            openFileDialog.DefaultExt = ".db";
+            openFileDialog.Filter = "Data Base File (.db) | *.db";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                UserPreferences.Default.LastDBPath = openFileDialog.FileName;
+            }
+
+        }
     }
 }
